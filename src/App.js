6186a06cb9452160cdd94c8fd2842ab34e458869
alexandraGoloskok1990 <alexandra.goloskok_new@anstrex.com>
2023-01-11 17:46:14 +0200

    End part

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import PetInfo from './Components/PetInfo';
 
 function App() {
   return (
     <div className="App">
-
+      <PetInfo animal={"Cat11"} />
     </div>
   );
 }
---
+++ src/Components/PetInfo.js
@@ -0,0 +1,7 @@
+function PetInfo(props) {
+    return (
+        <h1>This {props.animal} is 3 years ago</h1>
+    )
+}
+
+export default PetInfo
