ba714905f836689faa3318b9a84c0d518010cd97
alexandraGoloskok1990 <alexandra.goloskok_new@anstrex.com>
2023-01-13 09:01:46 +0200

    End part

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+
+  const [placeState, setPlaceState] = useState(null)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => {
+          setPlaceState(json)
+        })
+  },[])
+  console.log(placeState)
+
   return (
     <div className="App">
-
+      {
+        placeState
+          ? (<h1>{placeState.title}</h1>)
+          : (<div>Loading...</div>)
+      }
     </div>
   );
 }
