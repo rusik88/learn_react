7e624c1c4152b0b276e587ed138ab93f13f56941
alexandraGoloskok1990 <alexandra.goloskok_new@anstrex.com>
2023-01-13 10:41:29 +0200

    Ecnd part2

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 import './App.css';
-<<<<<<< HEAD
-import {useEffect, useState} from "react";
-=======
 import Posts from "./components/Posts";
->>>>>>> master
 
 function App() {
-
-  const [placeState, setPlaceState] = useState(null)
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then(response => response.json())
-        .then(json => {
-          setPlaceState(json)
-        })
-  },[])
-  console.log(placeState)
-
-  return (
-    <div className="App">
-<<<<<<< HEAD
-      {
-        placeState
-          ? (<h1>{placeState.title}</h1>)
-          : (<div>Loading...</div>)
-      }
-=======
-      <Posts />
->>>>>>> master
-    </div>
-  );
+    return (
+        <div className="App">
+            <Posts />
+        </div>
+    );
 }
 
 export default App;
